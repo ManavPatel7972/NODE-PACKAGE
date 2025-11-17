e98c8a5024f222f0696614351877af73a41c5457
Manav Delvadiya <24010101056@darshan.ac.in>
2025-11-17 21:36:14 +0530

    Updated Index File

--- bin/index.js
+++ bin/index.js
@@ -79,7 +79,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(\`\${APP_NAME} running on port \${PORT}\`));`
   );
 
-  console.log("\n✔ Scaffold Complete!");
+  console.log("\n Scaffold Complete!");
   console.log(`Next steps:
   cd ${targetName}
   npm init -y
