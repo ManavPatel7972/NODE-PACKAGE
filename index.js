0cac32d0419d6b9bc995b6a056e2a55d5ec1e558
Manav Delvadiya <24010101056@darshan.ac.in>
2025-11-27 18:36:30 +0530

    Fix Error in index.js file

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/',(req,res)=>{
     res.send("<h1>This Package Is Created By Manav Delvadiya</h1>");
 });
-app.listen(PORT, () => console.log(\`Server running on port ${PORT}\`));
+app.listen(PORT, () => console.log(\`Server running on port \${PORT}\`));
 module.exports = app;`
   );
 
